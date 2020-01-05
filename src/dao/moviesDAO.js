761291130ff0c639bbb3ab1689f540be14047d8c
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2020-01-05 11:40:04 +0100

    Add faceted search

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -202,6 +202,9 @@ export default class MoviesDAO {
       sortStage,
       // TODO Ticket: Faceted Search
       // Add the stages to queryPipeline in the correct order.
+      skipStage,
+      limitStage,
+      facetStage
     ]
 
     try {
